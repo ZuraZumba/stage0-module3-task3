376a99658131b29d3df4efd560b3b6c27df24ff8
zurazumba <zura.zumbadze@yahoo.com>
2023-06-13 16:21:42 +0400

    3-3

--- src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
+++ src/main/java/lang/print/gaps/task3/BasicVariablesInitialization.java
@@ -1,17 +1,16 @@
 package lang.print.gaps.task3;
-import java.util.logging.Logger;
 
 public class BasicVariablesInitialization {
-    private static final Logger logger = Logger.getLogger(BasicVariablesInitialization.class.getName());
 
     public static void main(String[] args) {
         int first = 1;
         int second = 10;
         int third = 100;
 
-        logger.info(String.valueOf(first));
-        logger.info(String.valueOf(second));
-        logger.info(String.valueOf(third));
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
     }
 }
 
--- src/main/java/lang/print/gaps/task3/DeclaringVars.java
+++ src/main/java/lang/print/gaps/task3/DeclaringVars.java
@@ -1,13 +1,12 @@
 package lang.print.gaps.task3;
-import java.util.logging.Logger;
-
 public class DeclaringVars {
-    private static final Logger logger = Logger.getLogger(DeclaringVars.class.getName());
 
     public static void main(String[] args) {
         int a = 10, b = 10, c = 10;
 
-        logger.info(String.valueOf(c));
+        int a, b, c;
+        a = b = c = 10;
+        System.out.println(c);
     }
 }
 
